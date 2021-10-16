1d468ece470955c482c780bd64a6a64c9a90533c
FurkanDGN <furkanaxx34@gmail.com>
2021-10-16 20:28:46 +0300

    dev.

--- src/main/java/com/gmail/furkanaxx34/rideablemobs/RideableMobsAPI.java
+++ src/main/java/com/gmail/furkanaxx34/rideablemobs/RideableMobsAPI.java
@@ -1,12 +1,10 @@
 package com.gmail.furkanaxx34.rideablemobs;
 
 import com.gmail.furkanaxx34.rideablemobs.task.RideableTeleportTask;
-import com.gmail.furkanaxx34.rideablemobs.util.ReflectionUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Locale;
 
 public class RideableMobsAPI {
@@ -22,7 +20,6 @@ public class RideableMobsAPI {
 
             Horse horse = (Horse) player.getWorld().spawnEntity(player.getLocation(), EntityType.HORSE);
             prepareHorse(horse, player);
-            changeMountedEntity(player, entity);
             new RideableTeleportTask(horse, mob).startTask();
         }
     }
@@ -34,16 +31,5 @@ public class RideableMobsAPI {
         horse.setPassenger(player);
         horse.setSilent(true);
         horse.setInvisible(true);
-    }
-
-    public void changeMountedEntity(Player player, Entity mob) {
-        try {
-            Class<?> packetPlayOutMount = ReflectionUtils.getNMSClass("PacketPlayOutMount");
-            Class<?> entity = ReflectionUtils.getNMSClass("Entity");
-            Object packet = packetPlayOutMount.getConstructor(entity).newInstance(mob.getClass().getMethod("getHandle").invoke(mob));
-            ReflectionUtils.sendPacket(player, packet);
-        } catch (InvocationTargetException | InstantiationException | IllegalAccessException | ClassCastException | NoSuchMethodException e) {
-            e.printStackTrace();
-        }
     }
 }
